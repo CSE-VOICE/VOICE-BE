c1bd1d8c6e328d54b88768fcf48bfea2ea66bda3
daina192939@gmail.com <daina192939@gmail.com>
2024-11-22 12:05:38 +0900

    [VOICE-22] 💫 feat(appliance): Enhance power control with validation

--- src/routes/appliance.routes.js
+++ src/routes/appliance.routes.js
@@ -4,7 +4,7 @@ const { applianceController } = require('../controllers');
 
 router.get('/', applianceController.getAllAppliances); // 기기 목록 조회 (/appliances?userId=1)
 router.get('/:applianceId', applianceController.getApplianceById); // 기기별 상세 조회 (/appliances/1?userId=1)
-// 기기별 수동제어는 이후 구현 예정
+router.patch('/:applianceId', applianceController.controlAppliancePower); // 기기별 전원 제어 (/appliances/1?userId=1)
 router.patch('/', applianceController.updateAppliances); // 기기 변동사항 업데이트
 
 module.exports = router;
